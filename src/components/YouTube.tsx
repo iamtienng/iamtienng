d7dbb70e8c62fc0c880e1c8ac443d78887fedd26
iamtienng <iamtienng@gmail.com>
2023-06-16 16:33:32 +0200

    Remove Import

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -1,13 +1,11 @@
-const styles = require("../styles/Youtube.module.css");
-
 export default function YouTube({ id }: { id: string }) {
   return (
-    <div className={styles.container}>
+    <div className="pb-96 relative h-0 overflow-hidden max-w-full">
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
-        className={styles.frame}
+        className="absolute top-0 left-0 w-full h-full"
       />
     </div>
   );
