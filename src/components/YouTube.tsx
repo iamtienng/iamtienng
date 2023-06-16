271f2e3fa66987950f66caf33e369031022d58a1
iamtienng <iamtienng@gmail.com>
2023-06-16 16:21:26 +0200

    Change Import Style

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -1,11 +1,13 @@
+const styles = require("../styles/Youtube.module.css");
+
 export default function YouTube({ id }: { id: string }) {
   return (
-    <div className="position: relative padding-bottom: 56.25% height: 0 overflow: hidden max-width: 100%">
+    <div className={styles.container}>
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
-        className="position: absolute top: 0 left: 0 width: 100% height: 100% border: 0"
+        className={styles.frame}
       />
     </div>
   );
