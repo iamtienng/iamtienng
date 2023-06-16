9ff534af63fc763b3e55274d1f640f4b75899fb2
iamtienng <iamtienng@gmail.com>
2023-06-16 16:13:32 +0200

    Deleted CSS Import

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -1,13 +1,11 @@
-import styles from "@/styles/Youtube.module.css";
-
 export default function YouTube({ id }: { id: string }) {
   return (
-    <div className={styles.container}>
+    <div className="position: relative padding-bottom: 56.25% height: 0 overflow: hidden max-width: 100%">
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
-        className={styles.frame}
+        className="position: absolute top: 0 left: 0 width: 100% height: 100% border: 0"
       />
     </div>
   );
